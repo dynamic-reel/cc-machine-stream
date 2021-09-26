f4d961f23c2904ebc3b777a41b7c5afed873e796
André Wetzel <andre@dynamic-reel.com>
2021-09-26 19:14:08 +0200

    some cleaning up

--- gui/src/app/data.service.ts
+++ gui/src/app/data.service.ts
@@ -2,13 +2,9 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { AuthenticationService } from '@app/_services';
-// import { environment } from '@environments/environment';
 import { environment } from 'src/environments/environment';
-// import { User, UserBasic } from '@global/_models/user/';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
-import { Machine, MachinesResponse } from './_models/machines.model';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Machine } from './_models/machines.model';
 
 
 @Injectable({
@@ -16,27 +12,8 @@ import { Machine, MachinesResponse } from './_models/machines.model';
 })
 export class DataService {
 
-//   private internalApiUrl: string = environment.internalApiUrl;
 private internalApiUrl: string = environment.internalApiUrl;
 
-
-//   optionalExceptionsListColumns: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(null);
-
-//   searchResponseHead: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-//   exceptionsList: BehaviorSubject<any[]> = new BehaviorSubject<any[]>(null);
-
-//   searchParams: BehaviorSubject<any> = new BehaviorSubject<any>({ search: null, no_scan: true, no_remediation: false });
-
-//   itemsPerPage: BehaviorSubject<number> = new BehaviorSubject<number>( 25 );
-//   currentPage: BehaviorSubject<number> = new BehaviorSubject<number>( 0 );
-
-//   createNewException: BehaviorSubject<boolean> = new BehaviorSubject<boolean>( false );
-//   newExceptionData: BehaviorSubject<any> = new BehaviorSubject<any>( null );
-
-//   exceptionsDataLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>( false );
-
-
-//   private subject = new Subject<any>();
 
     openedMachines: BehaviorSubject<any> = new BehaviorSubject<any>( {} );
 
@@ -45,23 +22,7 @@ private internalApiUrl: string = environment.internalApiUrl;
 
     constructor(
         private http: HttpClient,
-        // private authenticationService: AuthenticationService,
     ) { }
-
-
-//   // sendClickEvent() {
-//   //   this.subject.next();
-//   // }
-//   // getClickEvent(): Observable<any>{
-//   //   return this.subject.asObservable();
-//   // }
-
-//   startSearch() {
-//     this.subject.next();
-//   }
-//   getStartSearch(): Observable<any> {
-//     return this.subject.asObservable();
-//   }
 
 
     getHealth(): Observable<any> {
@@ -72,21 +33,6 @@ private internalApiUrl: string = environment.internalApiUrl;
         return this.http.get<any>(`${this.internalApiUrl}/machines`);
     }
 
-    // getMachines() {
-    //     this.http.get<any>(`${this.internalApiUrl}/machines`)
-    //     .pipe(
-    //         tap(data => console.log(data)),
-    //         // map( res => res.data)
-    //     )
-    //     // .subscribe(res => this.machines.next( res.data ));
-    //     .subscribe(res => {
-    //         res.data.map(item => {
-    //             //
-    //         });
-    //         this.machines.next( res.data );
-    //     });
-    // }
-
     getSingleMachine( itemid: string ): Observable<any> {
         return this.http.get<any>(`${this.internalApiUrl}/machines/${itemid}`);
     }
@@ -95,29 +41,6 @@ private internalApiUrl: string = environment.internalApiUrl;
         const item = this.openedMachines.value[machineid];
         item.data = data;
     }
-
-//   // getExceptions( gid: string, params: any ): Observable<any> {
-//   getExceptions(): Observable<any> {
-//     const params = {
-//       ...this.searchParams.value,
-//       itemsPerPage: this.itemsPerPage.value,
-//       page: this.currentPage.value,
-//       gid: this.authenticationService.userSubject.value.username,
-//     }
-//     return this.http.post<any>(`${this.internalApiUrl}/exceptions/list`, params);
-//   }
-
-//   // searchExceptions( searchparams: any ): Observable<any> {
-//   //   return this.http.post<any>(`${this.internalApiUrl}/exceptions/search`, searchparams);
-//   // }
-
-//   newException( data: any ): Observable<any> {
-//     return this.http.post<any>(`${this.internalApiUrl}/exceptions/new`, data);
-//   }
-
-//   editException( itemid: string | number, data: any ): Observable<any> {
-//     return this.http.post<any>(`${this.internalApiUrl}/exceptions/edit/${itemid}`, data);
-//   }
 
 }
 
